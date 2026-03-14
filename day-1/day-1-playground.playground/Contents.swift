9d55adff99d530e6f56e27857dbc35f5c18a41f6
jroll70 <jonryan42@gmail.com>
2026-03-13 21:54:09 -0600

    Updating with lesson 5 content

--- day-1/day-1-playground.playground/Contents.swift
+++ day-1/day-1-playground.playground/Contents.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// Lesson 4
 var name = "Ted"
 name = "Rebecca"
 name = "Keeley"
@@ -19,3 +21,27 @@ print(playerName)
 let managerName = "Michael Scott"
 let dogBreed = "Samoyed"
 let meaningOfLife = "How many roads must a man walk down?"
+
+// Lesson 5
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
