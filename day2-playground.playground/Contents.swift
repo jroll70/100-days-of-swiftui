d1d0dc0c58f5b0858fe9b077dc95a802b3a96363
jroll70 <jonryan42@gmail.com>
2026-03-16 13:37:57 -0600

    Adding lesson 1 content

--- day2-playground.playground/Contents.swift
+++ day2-playground.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// Lesson 1 Booleans
+let goodDogs = true
+let gameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameNotOver = false
+print(gameOver)
+
+gameNotOver.toggle()
+print(gameNotOver)
+
+// Lesson 2
+
+
